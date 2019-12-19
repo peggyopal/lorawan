b20a3757987b1f3c4abe437cb52c668baf608154
poa681 <peggy.anderson@usask.ca>
2019-12-19 16:18:11 -0600

    Create test that creates ClassCEDs - expect to fail

--- test/class-c-end-device-test-suite.cc
+++ test/class-c-end-device-test-suite.cc
@@ -17,42 +17,83 @@
 using namespace ns3;
 using namespace lorawan;
 
-NS_LOG_COMPONENT_DEFINE ("ClassCEndDeviceTestSuite");
+NS_LOG_COMPONENT_DEFINE ("ClassCEndDeviceLorawanMacTestSuite");
 
 /////////////////////////////
 // EndDeviceStatus testing //
 /////////////////////////////
 
-class ClassCEndDeviceTestSuite : public TestCase
+class ClassCEndDeviceTest : public TestCase
 {
 public:
-  ClassCEndDeviceTestSuite ();
-  virtual ~ClassCEndDeviceTestSuite ();
+  ClassCEndDeviceTest ();
+  virtual ~ClassCEndDeviceTest ();
 
 private:
   virtual void DoRun (void);
 };
 
 // Add some help text to this case to describe what it is intended to test
-ClassCEndDeviceTestSuite::ClassCEndDeviceTestSuite ()
+ClassCEndDeviceTest::ClassCEndDeviceTest ()
   : TestCase ("Verify that the ClassCEndDeviceLorawan operates as expected")
 {
 }
 
 // Reminder that the test case should clean up after itself
-ClassCEndDeviceTestSuite::~ClassCEndDeviceTestSuite ()
+ClassCEndDeviceTest::~ClassCEndDeviceTest ()
 {
 }
-
 // This method is the pure virtual method from class TestCase that every
 // TestCase must implement
 void
-ClassCEndDeviceTestSuite::DoRun (void)
+ClassCEndDeviceTest::DoRun (void)
 {
   NS_LOG_DEBUG ("ClassCEndDeviceTestSuite");
 
+  int numDevices = 1;
   // Create a bunch of actual devices
-  NetworkComponents components = InitializeNetwork (10, 0);
-
-  NodeContainer endDevices = components.endDevices;
+  NetworkComponents components = InitializeNetwork (numDevices, 0, 2);
 }
+
+
+/**************
+ * Test Suite *
+ **************/
+
+// The TestSuite class names the TestSuite, identifies what type of TestSuite,
+// and enables the TestCases to be run. Typically, only the constructor for
+// this class must be defined
+
+class ClassCEndDeviceLorawanMacTestSuite : public TestSuite
+{
+public:
+  ClassCEndDeviceLorawanMacTestSuite ();
+};
+
+ClassCEndDeviceLorawanMacTestSuite::ClassCEndDeviceLorawanMacTestSuite ()
+  : TestSuite ("class-c", UNIT)
+  // : TestSuite ("class-c-lora-end-device", UNIT)
+{
+  LogComponentEnable ("ClassCEndDeviceLorawanMacTestSuite", LOG_LEVEL_DEBUG);
+
+  // LogComponentEnable ("NetworkServer", LOG_LEVEL_ALL);
+  // LogComponentEnable ("NetworkStatus", LOG_LEVEL_ALL);
+  // LogComponentEnable ("NetworkScheduler", LOG_LEVEL_ALL);
+  // LogComponentEnable ("NetworkController", LOG_LEVEL_ALL);
+  // LogComponentEnable ("NetworkControllerComponent", LOG_LEVEL_ALL);
+  // LogComponentEnable ("LoraNetDevice", LOG_LEVEL_ALL);
+  // LogComponentEnable ("GatewayLorawanMac", LOG_LEVEL_ALL);
+  LogComponentEnable ("EndDeviceLorawanMac", LOG_LEVEL_ALL);
+  // LogComponentEnable ("EndDeviceLoraPhy", LOG_LEVEL_ALL);
+  // LogComponentEnable ("EndDeviceStatus", LOG_LEVEL_ALL);
+
+  // LogComponentEnableAll (LOG_PREFIX_FUNC);
+  // LogComponentEnableAll (LOG_PREFIX_NODE);
+  // LogComponentEnableAll (LOG_PREFIX_TIME);
+
+  // TestDuration for TestCase can be QUICK, EXTENSIVE or TAKES_FOREVER
+  AddTestCase (new ClassCEndDeviceTest, TestCase::QUICK);
+}
+
+// Do not forget to allocate an instance of this TestSuite
+static ClassCEndDeviceLorawanMacTestSuite lorawanTestSuite;
