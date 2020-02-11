945e2d2b9ade78fb5b377c1be4253904b2fa0a50
poa681 <peggy.anderson@usask.ca>
2020-02-11 10:08:11 -0600

    Add SendToPhy (copied from ClassA) with addition of checking class device type to set reply data rate

--- model/end-device-lorawan-mac.cc
+++ model/end-device-lorawan-mac.cc
@@ -326,8 +326,78 @@ EndDeviceLorawanMac::DoSend (Ptr<Packet> packet)
 }
 
 void
-EndDeviceLorawanMac::SendToPhy (Ptr<Packet> packet)
-{ }
+EndDeviceLorawanMac::SendToPhy (Ptr<Packet> packetToSend)
+{ 
+  /////////////////////////////////////////////////////////
+  // Add headers, prepare TX parameters and send the packet
+  /////////////////////////////////////////////////////////
+
+  NS_LOG_DEBUG ("PacketToSend: " << packetToSend);
+
+  // Data Rate Adaptation as in LoRaWAN specification, V1.0.2 (2016)
+  if (m_enableDRAdapt && (m_dataRate > 0)
+      && (m_retxParams.retxLeft < m_maxNumbTx)
+      && (m_retxParams.retxLeft % 2 == 0) )
+    {
+      m_txPower = 14; // Reset transmission power
+      m_dataRate = m_dataRate - 1;
+    }
+
+  // Craft LoraTxParameters object
+  LoraTxParameters params;
+  params.sf = GetSfFromDataRate (m_dataRate);
+  params.headerDisabled = m_headerDisabled;
+  params.codingRate = m_codingRate;
+  params.bandwidthHz = GetBandwidthFromDataRate (m_dataRate);
+  params.nPreamble = m_nPreambleSymbols;
+  params.crcEnabled = 1;
+  params.lowDataRateOptimizationEnabled = 0;
+
+  // Wake up PHY layer and directly send the packet
+
+  Ptr<LogicalLoraChannel> txChannel = GetChannelForTx ();
+
+  NS_LOG_DEBUG ("PacketToSend: " << packetToSend);
+  m_phy->Send (packetToSend, params, txChannel->GetFrequency (), m_txPower);
+
+  //////////////////////////////////////////////
+  // Register packet transmission for duty cycle
+  //////////////////////////////////////////////
+
+  // Compute packet duration
+  Time duration = m_phy->GetOnAirTime (packetToSend, params);
+
+  // Register the sent packet into the DutyCycleHelper
+  m_channelHelper.AddEvent (duration, txChannel);
+
+  //////////////////////////////
+  // Prepare for the downlink //
+  //////////////////////////////
+
+  // Switch the PHY to the channel so that it will listen here for downlink
+  m_phy->GetObject<EndDeviceLoraPhy> ()->SetFrequency (txChannel->GetFrequency ());
+
+  // Instruct the PHY on the right Spreading Factor to listen for during the window
+  // create a SetReplyDataRate function?
+  uint8_t replyDataRate = GetFirstReceiveWindowDataRate ();
+  if (m_deviceClass == CLASS_C) 
+  {
+    replyDataRate = GetSecondReceiveWindowDataRate ();
+    NS_LOG_DEBUG ("m_dataRate: " << unsigned (m_dataRate) <<
+                  ", m_rx1DrOffset: " << unsigned (m_rx1DrOffset) <<
+                  ", replyDataRate: " << unsigned (replyDataRate) << ".");
+  }
+  else
+  {
+    NS_LOG_DEBUG ("m_dataRate: " << unsigned (m_dataRate) <<
+                 ", m_rx1DrOffset: " << unsigned (m_rx1DrOffset) <<
+                 ", replyDataRate: " << unsigned (replyDataRate) << ".");
+  }
+
+  m_phy->GetObject<EndDeviceLoraPhy> ()->SetSpreadingFactor
+    (GetSfFromDataRate (replyDataRate));
+
+}
 
 //////////////////////////
 //  Receiving methods   //
